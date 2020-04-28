080f68736d3d1e6bd013c8d9f1a3c6933415e13b
visheshagrawal <48515800+visheshagrawal@users.noreply.github.com>
2020-04-28 20:59:11 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php  
-       $servername = "localhost";  
-       $username = "root";  
-       $password = "";  
-    //    $conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
-    //    $sql = mysqli_select_db ('stay_at_home',$conn) or die("unable to connect to database"); 
-    $mysqli = new mysqli("localhost","root","","stay_at_home");
-?> 
