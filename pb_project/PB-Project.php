13f3f29a02c8b713bf050b8559431ea5efb2d985
visheshagrawal <48515800+visheshagrawal@users.noreply.github.com>
2020-04-29 02:03:20 +0530

    Add files via upload

---
+++ pb_project/PB-Project.php
@@ -0,0 +1,130 @@
+
+<!-- <!DOCTYPE HTML> -->
+
+<html>
+
+	<head>
+		<title>QuarantineBuddy</title>
+
+		<link rel="stylesheet" type="text/css" href="PB-ProjectStyle.css">
+		
+	</head>
+
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a class="logo">
+									<span class="title">Hello There!</span>
+								</a>
+
+						</div>
+                    </header>
+                    
+                    <div id="wrapper">
+                    <div><span style="color:black">
+                    <div id="jokeDisplay">
+                    </div>
+                    <script>
+                    var jokes= ['Guess the number of programmers it takes to change a light bulb? Zero its a hardware problem','There are only 10 kinds of people in this world: those who know binary and those who don’t.','Real programmers count from 0.', 'Why did the programmer quit his job? Because he didnt get arrays.', 'A foo walks into a bar takes a look around and says Hello World','0 is false 1 is true right? 1','Things arent always #000000 and #FFFFFF.','What is the most used language in programming? Profanity','!False its funny because its True','You had me at Hello World','2b||!2b','Yesterday I changed the name on my wifi to Hack if you can. Today I found it named Challenge Accepted','A programmer is a person who fixed a problem that you didnt know you had in a way you dont understand','How can you tell if a computer geek is an extrovert? They stare at your shoes when you talk instead of their own.','I would love to change the world but they wont give me the source code.','If at first you dont succedd call it version 1.0','Computers make very fast very accurate mistakes','I farted in the Apple store and everyone got mad at me. Not my fault they dont have Windows.','Knock Knock... Whos there? Art... Art Who? R2D2','Hilarious and amazingly true thing: if a pizza has a radius (z) and a depth (a) that pizzas volume can be defined Pi*z*z*a.']
+
+
+                    var randomNumber=Math.floor(Math.random()*(20));
+                    document.getElementById('jokeDisplay').innerHTML="<marquee><b>"+jokes[randomNumber]+"</b></marquee>";
+                    //document.getElementById("insert").innerHTML = "<marquee>"++"</marquee>";
+                        
+
+                    </script>
+
+</div>  
+
+
+
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<h1>How are you feeling today?</h1>
+								<p>Maybe we can help life those spirits up :)</p>
+							</header>
+
+              <hr>
+							<section id="intro" class="main">
+								<div class="spotlight">
+									<div class="content">
+										<!-- START AUTO-INSERT DATASETS -->
+
+
+                  <div class="spotlight">
+                    <div class="content">
+                      <h2>Happy</h2>
+                      <p>Glad to hear</p>
+                      <h6><b>Symptoms: </b>TROLOLOL NO ONE IS HAPPY IN IIITD, CHOSE SOMETHING ELSE</h6>
+                      
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">News</a></li>
+                      </ul>
+
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">Puzzles</a></li>
+                      </ul>
+
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">Help someone else out</a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+
+                <hr>
+
+                <div class="spotlight">
+                  <div class="content">
+                    <h2>Okay</h2>
+                      <p>You can surely do better with a little help</p>
+
+                      <h6><b>Symptoms: </b>HJVDKWVCWVYWVDV</h6>
+                      
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">News</a></li>
+                      </ul>
+
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">Puzzles</a></li>
+                      </ul>
+
+                      <ul class="actions">
+                        <li><a target="_blank" href="https://link to our website" class="button">Help someone else out</a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+                <hr>
+
+                
+							</section>
+							
+							
+						</div>
+					</div>
+				
+
+					<footer id="footer">
+						<div class="inner">
+							<ul class="copyright">
+								<li>Made by Juhi Pandey, Vishesh Agrawal, Ankit Rana, Parth Garg, Sarthak Pal </li>
+							</ul>
+						</div>
+					</footer>
+
+
+
+			</div>
+	</body>
+</html>
