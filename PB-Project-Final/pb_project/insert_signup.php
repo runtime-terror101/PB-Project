3504c68f2632f765b4dfde6b61bdf6dc8d912940
visheshagrawal <48515800+visheshagrawal@users.noreply.github.com>
2020-05-15 04:04:22 +0530

    Add files via upload

---
+++ PB-Project-Final/pb_project/insert_signup.php
@@ -0,0 +1,49 @@
+<?php
+
+$age=$_GET['age'];
+$name=$_GET['name'];
+$email=$_GET['email'];
+$pwd=$_GET['pwd'];
+
+echo $age;
+echo $name;
+echo $email;
+echo $pwd;
+
+$link = mysqli_connect("localhost", "root", "", "stay_at_home");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+$sq2 = "SELECT * FROM users WHERE email_id='$email' ";
+$checkemailid = mysqli_query($link, $sq2);
+
+if (mysqli_num_rows($checkemailid) > 0) {
+    $message= "User already exists";
+    header("Location:signup.php?message=$message"); // Link him to the first page where he has option of both
+    exit(0);
+}
+else{
+
+    $sql = "INSERT INTO users (uname, email_id, upassword,age) VALUES ('$name','$email','$pwd','$age')";
+    if(mysqli_query($link, $sql)){
+        echo "Records inserted successfully.";
+        header("Location:login.php");
+        exit(0);
+    } else{
+        //echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        header("Location:signup.php");
+    }
+
+}
+
+
+
+
+
+
+
+
+?>
